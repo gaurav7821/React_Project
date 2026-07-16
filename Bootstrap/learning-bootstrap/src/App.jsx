c655b5569851a5d863f342099215af9d7a48b5a6
chaudharigaurav865@gmail.com <chaudharigaurav865@gmail.com>
2026-07-16 15:29:04 +0530

    Import bootstrap and learning some predefined css classes in bootstrap

---
+++ Bootstrap/learning-bootstrap/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+    </div>
+  )
+}
+
+export default App
